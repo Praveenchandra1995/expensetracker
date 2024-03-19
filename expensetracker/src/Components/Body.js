80087f6808f280631a804e3c993772d54e524510
Praveenchandra1995 <Praveenchandra2662@gmail.com>
2024-03-19 15:56:14 +0530

    created grid functionality

---
+++ expensetracker/src/Components/Body.js
@@ -0,0 +1,19 @@
+const Body = () => {
+  return (
+    <div className="container-fluid" id="menu-content">
+      <div className="navbar">
+        <div className="nav">
+          <li className="nav-item">
+            <a className="nav-link" href=" ">
+              Expenses
+            </a>
+          </li>
+        </div>
+      </div>
+      <div className="content">
+        <h2>Hello</h2>
+      </div>
+    </div>
+  );
+};
+export default Body;
