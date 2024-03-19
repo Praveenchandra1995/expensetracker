655feed91c35129d705fb88c2384fa3607541060
Praveenchandra1995 <Praveenchandra2662@gmail.com>
2024-03-19 16:01:14 +0530

    merged side nav bar with header

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import Body from "./Components/Body";
+import Header from "./Components/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <Body />
     </div>
   );
