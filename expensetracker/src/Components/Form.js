666355538e0aa04f22941de8a2475157f190c4a0
Praveenchandra1995 <Praveenchandra2662@gmail.com>
2024-03-27 15:09:31 +0530

    form ui created

--- expensetracker/src/Components/Form.js
+++ expensetracker/src/Components/Form.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Form() {
-  return <div>bolo</div>;
+  return(
+  <div className="container-fluid">
+  <form>
+    <div className="w-25 h-auto border border-1 border-dark-subtle">
+<select className="form-select w-25">
+  <option className="form-control">Mr</option>
+  <option className="form-control">Mrs</option>
+  <option className="form-control">Miss</option>
+  </select>    
+  <label className="form-label ps-2 pe-2">Name:</label>
+  <input type="text" name="name" className="form-control"/>
+  <label className="form-label ps-2 pe-2">Date:</label>
+  <input type="date" name="date" className="form-control"/>
+  <select className="form-select" id="traveltype">
+    <option className="form-control">
+      Stationary
+    </option>
+    <option className="form-control">
+      Travel
+    </option>
+    <option className="form-control">
+      Maintainance
+    </option>
+  </select>
+<button className="form-submit btn btn-danger text-white fw-bold">Submit</button>
+    </div>
+  </form>
+  </div>
+  )
+  
 }
 
-export default Form;
+export default Form
