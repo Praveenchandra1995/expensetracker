999a9562f2520f1ba21fb5c20825b08c5aed3555
Praveenchandra1995 <Praveenchandra2662@gmail.com>
2024-04-01 15:38:28 +0530

    added functionality in with reduce method

--- expensetracker/src/Components/SubmitedExpense.js
+++ expensetracker/src/Components/SubmitedExpense.js
@@ -5,31 +5,29 @@ function SubmitedExpense() {
   debugger;
   const expenses=useSelector((state)=>state.form.expenses)
 console.log(expenses);
+const totalAmount=expenses.reduce((total,expense)=>total+parseFloat(expense.amount),0)
   return (
     <div className='container-fluid w-100 h-50'>
       <table className='table '>
-        <tr>
-        <th className='column ps-3'>
-          <th className='ps-3 pe-3'>Name:</th>
-          <th className='ps-3 pe-3'>Date:</th>
-          <th className='ps-3 pe-3'>ExpenseType:</th>
-          <th className='ps-3 pe-3'>Amount:</th>
+        <thead className='column ps-4'>
+          <th className='ps-4 pe-4'>Name:</th>
+          <th className='ps-4 pe-4'>Date:</th>
+          <th className='ps-4 pe-4'>ExpenseType:</th>
+          <th className='ps-4 pe-4'>Amount:</th>
           {expenses.expenseType==="Travel"&&(
-            <th className='ps-3 pe-3'>FromPlace:</th>,
-            <th className='ps-3 pe-3'>ToPlace:</th>
+            <th className='ps-4 pe-4'>FromPlace:</th>,
+            <th className='ps-4 pe-4'>ToPlace:</th>
 
           )}
-        </th>
-        </tr>
-        <tbody>
+        </thead>
           {expenses.map((input)=>{
             return(
               <tbody>
                 <tr className='column ps-3'>
-                  <td className='ps-2 pe-2'>{input.name}</td>
-                  <td className='ps-2 pe-2'>{input.Date}</td>
-                  <td className='ps-2 pe-2'>{input.expenseType}</td>
-                  <td className='ps-2 pe-2'>{input.amount}</td>
+                  <td className=''>{input.name}</td>
+                  <td className=''>{input.date}</td>
+                  <td className=''>{input.expenseType}</td>
+                  <td className=''>{input.amount}</td>
                 </tr>
                 {expenses.expenseType==="Travel"&&(
             <tr>
@@ -43,7 +41,12 @@ console.log(expenses);
               </tbody>
             )
           })}
-        </tbody>
+          <tfoot>
+            <tr>
+              <td>Total Amount:{totalAmount}</td>
+              
+            </tr>
+          </tfoot>
       </table>
     </div>
   )
