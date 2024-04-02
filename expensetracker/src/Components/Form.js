8e2f71b82a286c78b54a0e6894f2ec9dfecd56d4
Praveenchandra1995 <Praveenchandra2662@gmail.com>
2024-04-02 08:50:06 +0530

    changes made

--- expensetracker/src/Components/Form.js
+++ expensetracker/src/Components/Form.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 
 function Form() {
-
   const [formData, setFormData] = useState({
     salutation: "",
     name: "",
@@ -13,94 +12,119 @@ function Form() {
     amount: "",
   });
 
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
 
-  const handleSubmit=(event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    //console.log("Form Data To Submit:",formData);
-    dispatch({type:"On_Submit",payload:formData});
+    dispatch({ type: "On_Submit", payload: formData });
     setFormData({
-    salutation: "",
-    name: "",
-    date: "",
-    expenseType: "",
-    fromPlace: "",
-    toPlace: "",
-    amount: "",
-    })
-    }
-  const handleTravelTypeChange = (event) => {
-const {value}=event.target;
-setFormData({
-  ...formData,
-  expenseType:value,
-})
-
-
+      salutation: "",
+      name: "",
+      date: "",
+      expenseType: "",
+      fromPlace: "",
+      toPlace: "",
+      amount: "",
+    });
   };
-  const handleInputChange=(event)=>{
-   const {name,value}=event.target;
-setFormData({
-  ...formData,
-[name]:value,  
-});
 
-  }
-  //console.log(formData)
-  return(
-  
-  <div className="container-fluid">
-  <form onSubmit={handleSubmit}>
-    <div className="w-100 h-auto p-3 border border-1 border-dark-subtle mt-2 d-flex flex-column justify-content-evenly">
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-  <div className="d-flex flex-row ms-2">
-  <select className="form-select w-25 " onChange={handleInputChange}
-value={formData.salutation}
-  >
-  <option className="form-control">Mr</option>
-  <option className="form-control">Mrs</option>
-  <option className="form-control">Miss</option>
-  </select>  
-  <label className="form-label ps-2 pe-2">Name:</label>
-  <input type="text" name="name" className="form-control w-50" value={formData.name} onChange={handleInputChange}/>
-</div>
-<div className="d-flex flex-row m-2">
-  <label className="form-label ps-2 pe-2">Date:</label>
-  <input type="date" name="date" className="form-control w-50" value={formData.date} onChange={handleInputChange} />
-  </div>
-  <div className="d-flex flex-row m-2">
-  <label for="expensetype">Expense Type:</label>
-  <select className="form-select w-50 ms-2" id="traveltype " onChange={handleTravelTypeChange} value={formData.expenseType} >
-    <option className="form-control">
-      Stationary
-    </option>
-    <option className="form-control">
-      Travel
-    </option>
-    <option className="form-control">
-      Maintainance
-    </option>
-
-  </select>
-  {formData.expenseType === "Travel" && (
-            <div className="d-flex flex-column m-2">  
-              <label htmlFor="Place">From Place:</label>
-              <input type="text" name="fromPlace" className="form-control w-50 ms-2" value={formData.fromPlace} onChange={handleInputChange}/>
-              <label htmlFor="Place2">To Place:</label>
-              <input type="text" name="toPlace" className="form-control w-50 ms-2" value={formData.toPlace} onChange={handleInputChange}/>
+  return (
+    <div className="container-fluid">
+      <form onSubmit={handleSubmit}>
+        <div className="w-100 h-auto p-3 border border-1 border-dark-subtle mt-2 d-flex flex-column justify-content-evenly">
+          <div className="d-flex flex-row ms-2">
+            <select
+              className="form-select w-25"
+              onChange={handleInputChange}
+              value={formData.salutation}
+              name="salutation"
+            >
+              <option value="Mr">Mr</option>
+              <option value="Mrs">Mrs</option>
+              <option value="Miss">Miss</option>
+            </select>
+            <label className="form-label ps-2 pe-2">Name:</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control w-50"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="d-flex flex-row m-2">
+            <label className="form-label ps-2 pe-2">Date:</label>
+            <input
+              type="date"
+              name="date"
+              className="form-control w-50"
+              value={formData.date}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="d-flex flex-row m-2">
+            <label htmlFor="expensetype">Expense Type:</label>
+            <select
+              className="form-select w-50 ms-2"
+              onChange={handleInputChange}
+              value={formData.expenseType}
+              name="expenseType"
+            >
+              <option value="Stationary">Stationary</option>
+              <option value="Travel">Travel</option>
+              <option value="Maintainance">Maintainance</option>
+            </select>
+          </div>
+          {formData.expenseType === "Travel" && (
+            <div>
+              <div className="d-flex flex-column m-2">
+                <label htmlFor="Place">From Place:</label>
+                <input
+                  type="text"
+                  name="fromPlace"
+                  className="form-control w-50 ms-2"
+                  value={formData.fromPlace}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="d-flex flex-column m-2">
+                <label htmlFor="Place2">To Place:</label>
+                <input
+                  type="text"
+                  name="toPlace"
+                  className="form-control w-50 ms-2"
+                  value={formData.toPlace}
+                  onChange={handleInputChange}
+                />
+              </div>
             </div>
           )}
-  </div>
-  <div className="d-flex flex-row m-2">  <label for="Amount">Amount:</label>
-    <input type="number" name="amount" className="form-control w-50 ms-2" value={formData.amount} onChange={handleInputChange}/>
-    </div>
+          <div className="d-flex flex-row m-2">
+            <label htmlFor="Amount">Amount:</label>
+            <input
+              type="number"
+              name="amount"
+              className="form-control w-50 ms-2"
+              value={formData.amount}
+              onChange={handleInputChange}
+            />
+          </div>
 
-<button className="form-submit btn btn-danger text-white fw-bold mt-2">Submit</button>
+          <button className="form-submit btn btn-danger text-white fw-bold mt-2">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
-  </div>
-  )
-  
+  );
 }
 
-export default Form
+export default Form;
